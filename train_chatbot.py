c4be1c80e5afe5913ced2c2c3533809ded6c783e
Vitor Nakayama <41307262+vtnaka@users.noreply.github.com>
2022-05-24 11:55:42 -0300

    Create training data
    Training data is a bag of words, randomized and creating lists for training:
    - X: patterns
    - Y: intents

--- train_chatbot.py
+++ train_chatbot.py
@@ -27,3 +27,31 @@ print(len(words), "unique lemmatized words", words)
 
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
+
+# create the training data
+training = []
+# create empty array for the output
+output_empty = [0]*len(classes)
+# training set, bag of words for every sentence
+for doc in documents:
+  # initializing bag of words
+  bag = []
+  # list of tokenized words for the pattern
+  word_patterns = doc[0]
+  # lemmatize each word - create base word, in attempt to represent related words
+  word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+  #create the bag of words array with 1, if word is found in current pattern
+  for words in words:
+    bag.append(1) if word in word_patterns else bag.append(0)
+    
+  # output is a '0' for each tag and '1' for current tag (for each pattern)
+  output_row = list(output_empty)
+  output_row[classes.index(doc[1])] = 1
+  training.append([bag, output_row])
+# suffle the features and make numpy array
+random,shuffle(training)
+training = np.array(training)
+# create training and testing lists. X - patterns, Y - intents
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+print("training data is created")
